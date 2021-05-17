90e53181538bdd8c18c8729272b4fd134222a813
Bryan Jansen <bryanjansen1@hotmail.com>
2021-05-17 15:14:50 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 /* 👇 Start writing your p5.js code here */
 
-var afstand = 50;
+var afstand = 80;
+/* Dit is een variabele die de waarde van de afstand tussen de lijnen opslaat. */
 
 function setup() {
   createCanvas(1500,1000);
   noLoop();
 }
+
+/* In de function setup wordt het canvas gemaakt die 1500 bij 1000px is. */
+
 
 function draw() {
   background('#40916c');
@@ -18,6 +22,11 @@ function draw() {
     }
   }
 }
+
+/*In deze loop vul ik het canvas op met de horizontale en verticale lijnen. Door de if else 
+zijn deze random op het canvas te zien. De afstand tussen deze lijnen blijft 80.
+Verder pas ik in function draw de dikte van de lijnen aan en geef het canvas een nieuwe achtergrondkleur.
+*/
 
 function drawRandomline(horizontaal, verticaal,afstand){
   if(random() > 0.5){
@@ -32,13 +41,16 @@ function drawRandomline(horizontaal, verticaal,afstand){
   }
 }
 
+/* In de function drawrandomline zorg ik ervoor dat het doolhof elke keer veranderd wanneer de gebruiker 
+de pagin refreshed.*/
+
 function mouseClicked(){
   console.log('Ik ben een stukje tekst');
-  afstand = afstand + 10;
+  afstand = afstand + 20;
   redraw();
 }
-/* de eerste keer dat de lus wordt uitgevoerd is de waarde van lijn 0.
-Elke keer als de loop wordt uitgevoerd telt hij er 50 op en stopt bij lijn = 
-700 of groter. */
+
+/* In deze function vind de interactie plaats. Wanneer de gebruiker op het doolhof drukt wordt er 
+20 bij de afstand opgeteld. De afstand is 80 en wordt door 1 mouseclick 100. */
 
 
